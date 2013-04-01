b303e3a6d0812c55f0f50432716fe75a4776c8a4
Gene Drabkin <gene.drabkin@gmail.com>
2013-03-31 23:02:43 -0700

    added more tests

---
+++ test/aggcat/aggcat_test.rb
@@ -0,0 +1,54 @@
+require 'test_helper'
+
+class AggcatTest < Test::Unit::TestCase
+  def setup
+    Aggcat.configure do |config|
+      config.issuer_id = 'issuer_id'
+      config.consumer_key = 'consumer_key'
+      config.consumer_secret = 'consumer_secret'
+      config.certificate_path = "#{fixture_path}/cert.key"
+    end
+  end
+
+  def test_configure
+    configurable = Aggcat.configure do |config|
+      config.issuer_id = 'issuer_id'
+      config.consumer_key = 'consumer_key'
+      config.consumer_secret = 'consumer_secret'
+      config.certificate_path = "#{fixture_path}/cert.key"
+    end
+    assert_equal 'issuer_id', configurable.instance_variable_get(:'@issuer_id')
+    assert_equal 'consumer_key', configurable.instance_variable_get(:'@consumer_key')
+    assert_equal 'consumer_secret', configurable.instance_variable_get(:'@consumer_secret')
+    assert_equal "#{fixture_path}/cert.key", configurable.instance_variable_get(:'@certificate_path')
+  end
+
+  def test_scope
+    client1 = Aggcat.scope('1')
+    assert_true client1.is_a?(Aggcat::Client)
+    assert_equal 'issuer_id', client1.instance_variable_get(:'@issuer_id')
+    assert_equal 'consumer_key', client1.instance_variable_get(:'@consumer_key')
+    assert_equal 'consumer_secret', client1.instance_variable_get(:'@consumer_secret')
+    assert_equal "#{fixture_path}/cert.key", client1.instance_variable_get(:'@certificate_path')
+    assert_equal '1', client1.instance_variable_get(:'@customer_id')
+    client2 = Aggcat.client
+    assert_equal client1, client2
+    client3 = Aggcat.scope('1')
+    assert_equal client1, client3
+    client4 = Aggcat.scope('2')
+    assert_not_equal client1, client4
+  end
+
+  def test_no_scope
+    exception = assert_raise(ArgumentError) { Aggcat.scope(nil) }
+    assert_equal('customer_id is required for scoping all requests', exception.message)
+  end
+
+  def test_client_api
+    stub_request(:post, Aggcat::Base::SAML_URL).to_return(:status => 200, :body => fixture('oauth_token.txt'))
+    Aggcat.scope('1')
+    stub_get('/institutions').to_return(:body => fixture('institutions.xml'), :headers => {:content_type => 'application/xml; charset=utf-8'})
+    response = Aggcat.institutions
+    assert_equal response[:response][:institutions][:institution][0][:institution_id].to_i, 100000
+  end
+end
