2c1bb0cb2b05cf7a2e1df7e7933bcff119588b7c
Michael Darmousseh <michael.darmousseh@gmail.com>
2014-05-15 13:40:38 -0700

    allow putting the certificate directly instead of requiring a path

--- lib/aggcat/configurable.rb
+++ lib/aggcat/configurable.rb
@@ -1,9 +1,9 @@
 module Aggcat
   module Configurable
 
-    attr_writer :issuer_id, :consumer_key, :consumer_secret, :certificate_path, :customer_id, :open_timeout, :read_timeout, :verbose
+    attr_writer :issuer_id, :consumer_key, :consumer_secret, :certificate_path, :certificate, :customer_id, :open_timeout, :read_timeout, :verbose
 
-    KEYS = [:issuer_id, :consumer_key, :consumer_secret, :certificate_path, :customer_id, :open_timeout, :read_timeout, :verbose]
+    KEYS = [:issuer_id, :consumer_key, :consumer_secret, :certificate_path, :certificate, :customer_id, :open_timeout, :read_timeout, :verbose]
 
     def configure
       yield self
@@ -15,6 +15,5 @@ module Aggcat
     def options
       Aggcat::Configurable::KEYS.inject({}) { |hash, key| hash[key] = instance_variable_get(:"@#{key}"); hash }
     end
-
   end
 end
